e1f40c20c8620490ebb9f6872759ee60fd008ad5
Jeyaroshini L S S <72503637+Jeyaroshini@users.noreply.github.com>
2022-09-08 17:52:22 +0530

    Update addTask.jsx

--- addTask.jsx
+++ addTask.jsx
@@ -2,14 +2,14 @@ import React from "react";
 class AddTask extends React.Component {
   constructor() {
     super();
-    this.handleInput = this.handleInput.bind(this)
-    this.addItem = this.addItem.bind(this)
-    this.deleteItem=this.deleteItem.bind(this)
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
     this.state = {
-      task: '',
-      status:false,
+      task: "",
+      status: false,
       submitValue: [],
-      id:Math.random()
+      id: Math.random(),
     };
   }
   handleInput = (event) => {
@@ -19,20 +19,19 @@ class AddTask extends React.Component {
   };
   addItem = (event) => {
     if (this.state.task !== "") {
-        const submitValue=[...this.state.submitValue];
-        submitValue.push(this.state.task)
-        this.setState({status:true, submitValue:submitValue, task:''})
-        console.log(this.state.task,this.state.id)
+      const submitValue = [...this.state.submitValue];
+      submitValue.push(this.state.task);
+      this.setState({ status: true, submitValue: submitValue, task: "" });
+      console.log(this.state.task, this.state.id);
     }
     event.preventDefault();
   };
-  deleteItem = (event) =>
-  {
-    const submitValue = [...this.state.submitValue]
-   const deleteList = submitValue.filter(this.state.task !== this.state.id)
-    this.setState({submitValue:deleteList,task:''})
-    event.preventDefault()
-  }
+  deleteItem = (event) => {
+    const submitValue = [...this.state.submitValue];
+    const deleteList = submitValue.filter(this.state.task !== this.state.id);
+    this.setState({ submitValue: deleteList, task: "" });
+    event.preventDefault();
+  };
   render() {
     return (
       <div>
@@ -40,11 +39,14 @@ class AddTask extends React.Component {
         <input value={this.state.task} onChange={this.handleInput}></input>
         <button onClick={this.addItem}>Add Task</button>
         <div>
-        { this.state.submitValue.map(item=>{ 
-            return (<div> {item}
-            <button onClick={this.deleteItem}>Delete</button>
-                </div>)
-        })}
+          {this.state.submitValue.map((item) => {
+            return (
+              <div>
+                {item}
+                <button onClick={this.deleteItem}>Delete</button>
+              </div>
+            );
+          })}
         </div>
       </div>
     );
