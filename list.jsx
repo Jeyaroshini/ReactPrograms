6e48ec357b662c039d69dd42bd9cbf9e02850e80
Jeyaroshini L S S <72503637+Jeyaroshini@users.noreply.github.com>
2022-09-13 17:59:35 +0530

    Update list.jsx

--- list.jsx
+++ list.jsx
@@ -1,22 +1,24 @@
-function Lists()
-{
-  const list = [{name:'Roshini',Rollno:'19IT07'},
-{name:"Madhu",Rollno:"19IT08"},
-{Name:"Jeyasree",Rollno:"19IT09"}]
-  return(
+function Lists() {
+  const list = [
+    { name: "Roshini", Rollno: "19IT07" },
+    { name: "Madhu", Rollno: "19IT08" },
+    { Name: "Jeyasree", Rollno: "19IT09" },
+  ];
+  return (
     <div>
       <h1>List in Reactjs</h1>
-     {list.map(item => <List info={item.name} info1={item.Rollno}/>)}
+      {list.map((item) => (
+        <List info={item.name} info1={item.Rollno} />
+      ))}
     </div>
-  )
+  );
 }
-function List(props)
-{
-  return(
-  <li>
-   My name is {props.info}
-   Rollno is {props.info1}
-  </li>
-  )
+function List(props) {
+  return (
+    <li>
+      My name is {props.info}
+      Rollno is {props.info1}
+    </li>
+  );
 }
-export default Lists
+export default Lists;
