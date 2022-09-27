468e1f267957ec7277891ca26a1a1c49290fd5c4
Jeyaroshini L S S <72503637+Jeyaroshini@users.noreply.github.com>
2022-09-27 18:16:00 +0530

    Update resumeBuilder.jsx

--- resumeBuilder.jsx
+++ resumeBuilder.jsx
@@ -1,7 +1,10 @@
-resumeBuilder.jsx
-
 import React from "react";
-import Education from "./education";
+import DatePick from "./weatherReport";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./front.css"
+library.add(faPlus);
 class ResumeBuilder extends React.Component {
   constructor() {
     super();
@@ -12,6 +15,15 @@ class ResumeBuilder extends React.Component {
       objective: "",
       viewStatus: false,
       skill: "",
+      eduStatus: false,
+      school: "",
+      city: "",
+      certiStatus: false,
+      certification: "",
+      internStatus: false,
+      position: "",
+      company: "",
+      project: "",
     };
     this.handleName = this.handleName.bind(this);
     this.handlemail = this.handlemail.bind(this);
@@ -19,6 +31,11 @@ class ResumeBuilder extends React.Component {
     this.handleObjective = this.handleObjective.bind(this);
     this.handleSkill = this.handleSkill.bind(this);
     this.handleView = this.handleView.bind(this);
+    this.handleEduStatus = this.handleEduStatus.bind(this);
+    this.handleSchool = this.handleSchool.bind(this);
+    this.handleCity = this.handleCity.bind(this);
+    this.handleCertiStatus = this.handleCertiStatus.bind(this);
+    this.handleCertifications = this.handleCertifications.bind(this);
   }
   handleName = (event) => {
     this.setState({
@@ -56,44 +73,156 @@ class ResumeBuilder extends React.Component {
     });
     event.preventDefault();
   };
-
+  handleEduStatus = (event) => {
+    this.setState({
+      eduStatus: true,
+    });
+    event.preventDefault();
+  };
+  handleSchool = (event) => {
+    this.setState({
+      school: event.target.value,
+    });
+  };
+  handleCity = (event) => {
+    this.setState({
+      city: event.target.value,
+    });
+  };
+  handleCertiStatus = (event) => {
+    this.setState({
+      certiStatus: true,
+      eduStatus:false
+    });
+    event.preventDefault();
+  };
+  handleCertifications = (event) => {
+    this.setState({
+      certification: event.target.value,
+    });
+    event.preventDefault();
+  };
+  internStatus = (event) => {
+    this.setState({
+      internStatus: true,
+    });
+    event.preventDefault();
+  };
+  handlePosition = (event) => {
+    this.setState({
+      position: event.target.value,
+    });
+  };
+  handleCompanyName = (event) => {
+    this.setState({
+      company: event.target.value,
+    });
+  };
+  handleProject = (event) => {
+    this.setState({
+      project: event.target.value,
+    });
+  };
   render() {
     return (
-      <div>
+      <div className="design">
         <form>
-          Name:{" "}
+          <table>
+          <tr><td>
+          Name:{" "}</td><td>
           <input
             placeholder="Enter Your Name"
             onChange={this.handleName}
-          ></input>
+          ></input></td></tr>
           <br />
-          E-Mail:{" "}
+          <tr><td>
+          E-Mail:{" "}</td><td>
           <input
             placeholder="Enter Your E-mail"
             onChange={this.handlemail}
-          ></input>
-          <br />
-          Number:{" "}
+          ></input></td></tr>
+          <br /><tr><td>
+          Number:{" "}</td><td>
           <input
             placeholder="Enter Your Phone Number"
             onChange={this.handlePhoneNumber}
-          ></input>
-          <br />
-          Career Objective:{" "}
+          ></input></td></tr>
+          <br /><tr><td>
+          Career Objective:{" "}</td><td>
           <textarea
             placeholder="Enter Your Career Objective"
             onChange={this.handleObjective}
-          ></textarea>
-          <br />
-          Skills:{" "}
+          ></textarea></td></tr>
+          <br /><tr><td>
+          Skills:{" "}</td><td>
           <textarea
             placeholder="Enter your Skills"
             onChange={this.handleSkill}
-          ></textarea>
+          ></textarea></td></tr>
           <br />
-          Educational Qualification: <Education />
+          <tr><td>
+          <button onClick={this.handleEduStatus} >
+            <FontAwesomeIcon icon="plus"></FontAwesomeIcon>Educational
+            Qualification{" "}
+          </button>
+          
+          <button onClick={this.handleCertiStatus}>
+            <FontAwesomeIcon icon="plus"></FontAwesomeIcon>Certificate
+          </button>
+         
+          <button onClick={this.internStatus}>
+            <FontAwesomeIcon icon="plus"></FontAwesomeIcon>Internship
+          </button></td></tr>
+          <div>
+            {this.state.eduStatus && (
+              <form>
+                <table>
+                <tr><td>
+                School:</td><td> <input onChange={this.handleSchool}></input></td></tr>
+                <br />
+                <tr><td>
+                City:</td><td><input onChange={this.handleCity}></input></td></tr>
+                <br />
+                <tr><td>
+                Start Date:</td><td><DatePick /></td></tr>
+                <br />
+                <tr><td>
+                End Date: </td><td><DatePick /> <br /></td></tr>
+                </table>
+              </form>
+            )}
+          </div>
+          <div>
+            {this.state.certiStatus && (
+              <form>
+                <table>
+                <tr><td>
+                Certifications:{" "}</td><td>
+                <input onChange={this.handleCertifications}></input></td></tr>
+                </table>
+              </form>
+            )}
+          </div>
+          <div>
+            {this.state.internStatus && (
+              <form>
+                <table>
+                <tr><td>
+                Position: </td><td><input onChange={this.handlePosition}></input></td></tr>
+                <br />
+                <tr><td>
+                Company Name:</td><td> <input onChange={this.handleCompanyName}></input></td></tr>
+                <br />
+                <tr><td>
+                Project:</td><td> <input onChange={this.handleProject}></input> <br /></td></tr>
+                </table>
+              </form>
+            )}
+          </div>
+          </table>
         </form>
-        <button onClick={this.handleView}>View Resume</button>
+        <tr><td>
+        <button onClick={this.handleView}>View Resume</button></td></tr>
         <div>
           {this.state.viewStatus && (
             <p>
@@ -112,6 +241,23 @@ class ResumeBuilder extends React.Component {
               <b>Skills</b> <br />
               {this.state.skill}
               <hr />
+              <b>Education</b>
+              <br />
+              {this.state.school}
+              <br />
+              {this.state.city} <br />
+              <hr />
+              <b>Certification</b>
+              <br />
+              {this.state.certification}
+              <br />
+              <hr />
+              <b>Internships</b>
+              <br />
+              {this.state.position}
+              <br />
+              {this.state.company} <br />
+              {this.state.project} <br />
             </p>
           )}
         </div>
@@ -120,48 +266,3 @@ class ResumeBuilder extends React.Component {
   }
 }
 export default ResumeBuilder;
-
-
-education.jsx:
-
-import React from "react";
-class Education extends React.Component {
-  constructor() {
-    super();
-    this.state = { degree: "", stream: "", graduation: "" };
-    this.handleDegree = this.handleDegree.bind(this);
-    this.handleStream = this.handleStream.bind(this);
-    this.handlegraduation = this.handlegraduation.bind(this);
-  }
-  handleDegree = (event) => {
-    this.setState({
-      degree: event.target.value,
-    });
-  };
-  handleStream = (event) => {
-    this.setState({
-      stream: event.target.value,
-    });
-  };
-  handlegraduation = (event) => {
-    this.setState({
-      graduation: event.target.value,
-    });
-  };
-  render() {
-    return (
-      <div>
-        <form>
-          Degree: <input onChange={this.handleDegree}></input>
-          <br />
-          Stream: <input onChange={this.handleStream}></input>
-          <br />
-          Year of Graduation: <input onChange={this.handlegraduation}></input>
-          <br />
-        </form>
-      </div>
-    );
-  }
-}
-export default Education;
-
