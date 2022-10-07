459e67263630a11d9cc547a4dedef81ffdf74f28
Jeyaroshini L S S <72503637+Jeyaroshini@users.noreply.github.com>
2022-10-07 17:30:50 +0530

    Update crud.jsx

--- crud.jsx
+++ crud.jsx
@@ -1,87 +1,63 @@
-import React from "react";
-class Update extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      input: "",
-      status: false,
-      updateInput: "",
-      inputs: [],
-      id: null,
+import React, { useState } from "react";
+function Update() {
+  const [input, setInput] = useState("");
+  const [inputs, setInputs] = useState([]);
+  const [editInput, setEditInput] = useState(null);
+  const [editTask, setEditTask] = useState("");
+  const handleInput = (event) => {
+    setInput(event.target.value);
+  };
+  const addItem = (event) => {
+    event.preventDefault();
+    const newItem = {
+      id: Math.random(),
+      text: input,
     };
-    this.handleInput = this.handleInput.bind(this);
-    this.addTask = this.addTask.bind(this);
-    this.handleUpdate = this.handleUpdate.bind(this);
-    this.updateInput = this.updateInput.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  handleInput = (event) => {
-    this.setState({
-      input: event.target.value,
-    });
-    event.preventDefault();
+    setInputs([...inputs].concat(newItem));
+    setInput("");
   };
-  addTask = (event) => {
-    const inputs = [...this.state.inputs];
-    inputs.push(this.state.input);
-    this.setState({
-      id: Math.floor(Math.random() * 10000),
-      inputs: inputs,
-      input: "",
-    });
-    console.log(this.state.input, this.state.id);
-    event.preventDefault();
+  const deleteItem = (id) => {
+    let deleteItem = [...inputs].filter((item) => item.id !== id);
+    setInputs(deleteItem);
   };
-  deleteItem = (id) => {
-    this.setState({
-      inputs: this.state.inputs.filter((item) => item.id !== id),
+  const submitEdit = (id) => {
+    const newItem = [...inputs].map((item) => {
+      if (item.id === id) {
+        item.task = editTask;
+      }
+      return item;
     });
+    setInputs(newItem);
+    setEditInput(null);
   };
-  handleUpdate = (event) => {
-    this.setState({
-      status: true,
-    });
-    event.preventDefault();
-  };
-  updateInput = (event) => {
-    this.setState({
-      updateInput: event.target.value,
-    });
-    event.preventDefault();
-  };
-  handleSubmit = (event) => {
-    this.setState({
-      input: this.state.updateInput,
-      updateInput: "",
-      status: false,
-    });
-    console.log(this.state.input);
-    event.preventDefault();
-  };
-  render() {
-    return (
-      <div>
-        <form>
-          Input: <input onChange={this.handleInput}></input>
-          <button onClick={this.addTask}>Add Task</button>
-          {this.state.inputs.map((item) => {
-            return (
+  return (
+    <div>
+      <h1>CRUD Operation</h1>
+      <form>
+        Enter Task: <input value={input} onChange={handleInput}></input>
+        <button onClick={addItem}>Add Task</button>
+        {inputs.map((item) => {
+          return (
+            <div>
+              {item.id === editInput ? (
+                <input onChange={(event) => setEditTask(event.target.value)}
+                ></input>
+              ) : (
+                <div>{item.text}</div>
+              )}
               <div>
-                {item}
-                <button onClick={() => this.deleteItem(item.id)}>Delete</button>
-                <button onClick={this.handleUpdate}>Update</button>
-                {this.state.status && (
-                  <form>
-                    Input: <input onChange={this.updateInput}></input>
-                    <button onClick={this.handleSubmit}>Submit</button>
-                  </form>
-                )}
+              {item.id === editInput ? (
+                <button onClick={() => submitEdit(item.id)}>Submit</button>
+              ) : (
+                <button onClick={(event) => setEditInput(item.id,event.preventDefault())}>Edit</button>
+              )}
+              <button onClick={() => deleteItem(item.id)}>Delete</button>
               </div>
-            );
-          })}
-        </form>
-      </div>
-    );
-  }
+            </div>
+          );
+        })}
+      </form>
+    </div>
+  );
 }
 export default Update;
