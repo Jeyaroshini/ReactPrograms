295d2e4bc379a87768c56fe077ca6d2f8e0ec1a9
Jeyaroshini L S S <72503637+Jeyaroshini@users.noreply.github.com>
2022-10-10 17:38:38 +0530

    Update LeaveApplicatiom.jsx

--- LeaveApplicatiom.jsx
+++ LeaveApplicatiom.jsx
@@ -1,80 +1,112 @@
-LeaveApplication.jsx :
-
-import React from 'react';
-class LeaveApplication extends React.Component
-{
-    constructor()
-    {
-        super()
-       this.state = ({employeeName:" ", noOfDaysTakenLeave: "", numberOfDaysToGetLeave:"",fixedSalary:50000 })
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+function Mail() {
+  const [otp, setotp] = useState(0);
+  const [status, setStatus] = useState(false);
+  const [input, setInput] = useState("");
+  const [start, setStart] = useState(true);
+  const [date, setDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const sendmail = (event) => {
+    event.preventDefault();
+    emailjs
+      .sendForm(
+        "service_akksl58",
+        "template_in9b83c",
+        event.target,
+        "5agA4Qxcu1pE1y335"
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    let otp = Math.floor(Math.random() * 10000);
+    setotp(otp);
+    setStatus(true);
+    console.log(otp);
+  };
+  const handleLeave = (event) => {
+    event.preventDefault();
+    emailjs
+      .sendForm(
+        "service_akksl58",
+        "template_zlyjewi",
+        event.target,
+        "5agA4Qxcu1pE1y335"
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const handleInput = (event) => {
+    setInput(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    if (otp === Number(input)) {
+      setStart(false);
+    } else {
+      alert("Invalid Verification Code");
     }
-
-name = (event) =>
-{
-    this.setState({
-        employeeName: event.target.value
-    })
+    event.preventDefault();
+  };
+  return (
+    <div>
+      {start && (
+        <form onSubmit={sendmail}>
+          <label>Name</label>
+          <input type="text" name="username" required></input>
+          <br />
+          <label>Email-ID</label>
+          <input type="text" name="email-id" required></input>
+          <br />
+          {status && (
+            <form>
+              <label>Enter OTP</label>
+              <input
+                name="otp-otp"
+                type="number"
+                onChange={handleInput}
+              ></input>
+              <button onClick={handleSubmit}>Submit</button>
+            </form>
+          )}
+          <button>Get OTP</button>
+        </form>
+      )}
+      {!start && (
+        <div>
+          <h1>Welcome to Leave Application</h1>
+          <form onSubmit={handleLeave}>
+            <label>Name</label>
+            <input name="username" type="text"></input>
+            <label>Mail-ID</label>
+            <input type="email" name="email"></input>
+            <label>Start Date</label>
+            <DatePicker
+              selected={date}
+              onChange={(date) => setDate(date)}
+              name="startDate"
+            />
+            <label>End Date</label>
+            <DatePicker
+              selected={endDate}
+              onChange={(date) => setEndDate(date)}
+              name="endDate"
+            />
+            <label>Reason for leave</label>
+            <textarea type="text" name="reason"></textarea>
+            <button>Submit</button>
+          </form>
+        </div>
+      )}
+    </div>
+  );
 }
-
-noOfDaysTakenLeave = (event) =>
-{
-    this.setState({
-        noOfDaysTakenLeave: event.target.value
-    })
-}
-
-calculate = (event) =>
-{
-    if(this.state.noOfDaysTakenLeave > 3)
-    {
-        alert("No more Leave")
-    }
-    else
-    {
-        alert("You can Proceed")
-    }
-    event.prevent.default()
-}
-numberOfDaysToGetLeave = (event) =>
-{
-    this.setState({
-        numberOfDaysToGetLeave: event.target.value
-    })
-}
-proceed = (event) =>
-{
-  if(this.state.numberOfDaysToGetLeave < 3) 
-  {
-    alert("You can proceed with leave")
-  }
-  
-  event.prevent.default()
-}
-viewSalary = () =>
-{
-    alert("You Current salary is: " ,((this.state.fixedSalary) - (this.numberOfDaysToGetLeave + 1000)))
-}
-
-
-    render()
-    {
-        return(
-            <div>
-                <h1>Welcome to Employee Leave Application</h1>
-                <form>
-                    EmployeeName:<input value={this.state.employeeName} onChange={this.name}></input>
-                    NoOfDaysTakenLeave:<input value={this.state.noOfDaysTakenLeave} onChange={this.noOfDaysTakenLeave}></input>
-                    <button onClick={this.calculate}>Calculate</button>
-                    <div>
-                     Wanted Leave:   <input value={this.state.numberOfDaysToGetLeave} onChange={this.numberOfDaysToGetLeave}></input>
-                    </div>
-                    <div>
-                        <button onClick={this.proceed}>Proceed</button>
-                    </div>
-                <button onClick={this.viewSalary}>View Salary</button>
-                </form>
-            </div>
-        )
-    }
-}
-export default LeaveApplication
+export default Mail;
